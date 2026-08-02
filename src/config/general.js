11d1949f1225682d66e8363038434dc4fc67e9b1
kian-007 <79346418+kian-007@users.noreply.github.com>
2026-08-02 10:11:54 +0330

    Update general.js

--- src/config/general.js
+++ src/config/general.js
@@ -1,4 +1,4 @@
-const protocol = window.location.protocol;
+/*const protocol = window.location.protocol;
 const domain = window.location.hostname;
 const port = window.location.port;
 
@@ -6,3 +6,8 @@ const port = window.location.port;
 const PROJECT_URL = `${protocol}//${domain}:${port? port : ""}`
 
 export { PROJECT_URL }
+*/
+
+const PROJECT_URL = process.env.PUBLIC_URL;
+
+export { PROJECT_URL };
